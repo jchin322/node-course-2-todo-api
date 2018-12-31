d739b2eed13a5dbc5703275efae9742e051a2c44
Jeffrey Chin <jeffreychin@uchicago.edu>
2018-12-31 14:31:58 -0500

    Setup separate test and development envs

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -54,7 +54,7 @@ describe('POST /todos', () => {
             if (err) {
                 return done(err);
             }
-            Todo.find().then((todos) => {
+            Todo.find({}).then((todos) => {
                 expect(todos.length).toBe(2);
                 done();
             }).catch((e) => done(e));
